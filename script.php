0b6bda1307dc27f6304e054dae86c5fa83ea5f50
Mihail Ershov <mihershov9@gmail.com>
2017-04-23 22:22:16 +0300

    php code was edited to be more logical and readable

--- script.php
+++ script.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Ershov</title>
-    <style>
-        * {
-            font-size: 1.5rem;
-            font-family: sans-serif;
-        }
-    </style>
-</head>
-<body>
-<form action="script.php">
-    <input type="value" placeholder="Введите число" name="number">
-    <input type="submit">
-</form>
-
 <?php
 
 error_reporting(E_ALL);
@@ -36,20 +18,32 @@ function fibonacci($x) {
 
 if (isset($_GET['number'])) {
     $num = $_GET['number'];
-} else {
-    $num = '';
+    $inNumberRow = "задуманное число " . $num . " входит в числовой ряд фибоначчи";
+    $notInNumberRow = "задуманное число " . $num . " не входит в числовой ряд фибоначчи";
 }
 
 ?>
-<p><?php
-    if ($num != null) {
-        if (fibonacci($num)) {
-            echo "задуманное число " . $num . " входит в числовой ряд фибоначчи";
-        } else {
-            echo "задуманное число " . $num . " не входит в числовой ряд фибоначчи";
-        };
-    };
-    ?></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ershov</title>
+    <style>
+        * {
+            font-size: 1.5rem;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+<form action="script.php">
+    <input type="value" placeholder="Введите число" name="number" required>
+    <input type="submit">
+</form>
+
+<?php if (isset($num)) : ?>
+    <p><?php echo (fibonacci($num) ? $inNumberRow : $notInNumberRow); ?></p>
+<?php endif; ?>
 
 </body>
 </html>
